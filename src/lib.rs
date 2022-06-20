dc8fa9cb77f6e18b45ab1a16d2a7e213812a2976
Nathan McCarty <nathan@mccarty.io>
2022-06-20 03:11:59 -0400

    feat: Add Metadata type

--- src/lib.rs
+++ src/lib.rs
@@ -15,15 +15,18 @@
 )]
 
 use serde::{Deserialize, Serialize};
-use types::Versions;
 
 pub mod types;
+
+use types::{Metadata, Versions};
 
 /// High level representation of a modpack
 ///
 /// TODO: Document
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Hash, Default)]
 pub struct Pack {
+    /// The metadata for this pack
+    pub metadata: Metadata,
     /// The versions of minecraft and the launcher that this pack works with
     pub versions: Versions,
 }
--- src/types.rs
+++ src/types.rs
@@ -9,6 +9,7 @@ mod minecraft;
 pub use loader::Loader;
 pub use minecraft::Minecraft;
 
+use semver::Version;
 use serde::{Deserialize, Serialize};
 
 /// The versions of minecraft and the launcher for this instance of the pack
@@ -20,4 +21,29 @@ pub struct Versions {
     pub minecraft: Minecraft,
     /// The loader this pack works with
     pub loader: Loader,
+}
+
+/// The metadata for the pack
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Hash)]
+pub struct Metadata {
+    /// The name of this mod pack
+    name: String,
+    /// An optional description for this mod pack
+    #[serde(skip_serializing_if = "Option::is_none")]
+    description: Option<String>,
+    /// The author of the pack
+    author: String,
+    /// The version of the pack
+    version: Version,
+}
+
+impl Default for Metadata {
+    fn default() -> Self {
+        Self {
+            name: "My super cool modpack!".to_string(),
+            description: Some("Totally a real mod pack!".to_string()),
+            author: "Your name here!".to_string(),
+            version: Version::parse("0.0.1").unwrap(),
+        }
+    }
 }
